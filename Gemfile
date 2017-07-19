77ee4ebd486bc21465edfc6d49229cef662c9844
David Neto <davidneto92@gmail.com>
2017-07-19 15:56:30 -0400

    users/sign_up working, but that's it

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'devise'
 
 group :development, :test do
   gem 'pry-byebug'
+  gem 'pry-rails'
   gem 'rspec-rails'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount_ember_app :frontend, to: "/"
+  # resources :users
 end
