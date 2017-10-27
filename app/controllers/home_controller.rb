f37d74bd94c660797e9ca24e3417d0b1c9ea7590
Brian Tenggren <btenggren20@gmail.com>
2017-10-26 20:06:11 -0400

    Add Home as the entry point for the app
    Goal:
    - Add a default home page with a controller
    - set that to the default route

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,4 @@
+class HomeController < ApplicationController
+  # Just render the show page as the home page for the site
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#show'
 
   resources :spells
-  # get 'spells/search', to: 'spells#search'
-
 end
